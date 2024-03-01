af71b77a4ea98cd5809d28827a7b746b41795b1e
sarfarazsaba11 <155134722+sarfarazsaba11@users.noreply.github.com>
2024-02-29 23:50:26 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,225 @@
+import inquirer from "inquirer";
+import chalk from 'chalk';
+
+let loop = true;
+let allStudents:{
+    name:string,
+    Age:number,
+    Id:string|number|undefined,
+    Percentage:number,
+    Grade:string}[]=[];
+
+
+
+
+function grade(marks:number){
+    if(marks>=80){
+        return 'Grade A-1';
+    }
+    else if(marks<80 && marks>=70){
+        return 'Grade A';
+    }
+    else if(marks<70 && marks >= 60){
+        return 'Grade B';
+    }
+    else if(marks<60 && marks >= 50){
+        return 'Grade C';
+    }else if(marks<50 && marks >=40){
+        return 'Grade D';
+    }
+    else{
+        return 'Fail';
+    }
+}
+
+
+console.log(chalk.bgBlueBright('\n*** Student Management System ***\n'))
+
+async function sMs(array:any[]){
+
+do{
+    let ans = await inquirer.prompt({
+        name:'choices',
+        type:'list',
+        choices:['1. New Student Form','2. Search Student by ID','3. Show all Students','4. Update Record','5. Delete Student Record','6. Exit']
+    });
+
+
+if(ans.choices === '1. New Student Form'){
+    var newStd:{
+        name:string,
+        Age:number,
+        Id:string|number|undefined,
+        Percentage:number,
+        Grade:string
+    } = await inquirer.prompt([{
+        type:'input',
+        name:'name',
+        message:chalk.green('Enter your Name: ')
+    },
+  
+    {
+        type:'number',
+        name:'Age',
+        message:chalk.green('Enter your age: '),
+        when(newStd){
+            return newStd.name
+        }
+    },
+    {
+        type:'input',
+        name:'Id',
+        message:chalk.green('Enter Student ID: '),
+        when(newStd){
+            return newStd.Age
+        }
+    },
+    {
+        type:'number',
+        name:'Percentage',
+        message:chalk.green('Enter your Percentage: '),
+        when(newStd){
+            return newStd.Id
+        },
+    },
+        
+]); 
+
+newStd.Grade = grade(newStd.Percentage);
+allStudents.push(newStd);
+console.log(chalk.redBright('\n****  Data Entered Successfully  ****'))
+};
+
+
+
+
+
+
+if(ans.choices === '2. Search Student by ID'){
+    
+    let enterId = await inquirer.prompt({
+        type:'input',
+        name:'enterId',
+        message:'Enter Student ID: '
+    });
+
+    for (let i = 0 ; i<= allStudents.length;i++){
+         if(enterId.enterId == allStudents[i].Id){
+            console.log(allStudents[i])
+            break ;
+            }
+            
+        }
+    }  
+
+
+
+
+
+if (ans.choices === '3. Show all Students'){
+    console.log(allStudents);
+}
+
+
+
+if(ans.choices === '4. Update Record'){
+
+    let updateRecord = await inquirer.prompt({
+        type:'input',
+        name:'enterId',
+        message:'Enter Student ID: '
+    });
+    for(let a=0;a<=allStudents.length;a++)
+    {
+        if(updateRecord.enterId == allStudents[a].Id){
+         let update = await inquirer.prompt({
+            type:'list',
+            name:'update',
+            choices:['Update Name','Update Age','Update Percentage'],
+            message:'Select'
+         });
+
+            if(update.update === 'Update Name'){
+
+                let updateName = await inquirer.prompt({
+                type:'input',
+                name:'updateName',
+                message:'Enter Name: '
+                 });
+
+                allStudents[a].name = updateName.updateName
+                console.log(chalk.redBright('\n*** Record Updated Successfully ***\n'))
+                break;
+                 }
+
+                 if(update.update === 'Update Age'){
+
+                    let updateAge = await inquirer.prompt({
+                        type:'input',
+                        name:'updateAge',
+                        message:'Enter Age: '
+                         })
+        
+                        allStudents[a].Age = updateAge.updateAge;
+                        console.log(chalk.redBright('\n*** Record Updated Successfully ***\n'))
+                        break;
+
+                 }
+
+                 if(update.update === 'Update Percentage'){
+
+                    let updatePer = await inquirer.prompt({
+                        type:'number',
+                        name:'updatePer',
+                        message:'Enter Percentage: '
+                         })
+
+                         allStudents[a].Percentage = updatePer.updatePer;
+                         allStudents[a].Grade = grade(updatePer.updatePer);
+                         console.log(chalk.redBright('\n*** Record Updated Successfully ***\n'))
+                         break;
+
+                 }
+    
+                break;
+    }
+}     
+}
+
+
+
+
+
+
+if(ans.choices === '5. Delete Student Record'){
+
+
+    let deleteRecord = await inquirer.prompt({
+        type:'input',
+        name:'enterId',
+        message:'Enter Student ID: '
+    });
+
+
+    for (let p=0; p<= allStudents.length;p++) {
+
+        if(deleteRecord.enterId == allStudents[p].Id){
+           delete allStudents[p];
+           console.log(chalk.red('\n*** Student Record Deleted Successfully ***\n'))
+           break;
+        }
+    }
+}
+
+
+
+
+if(ans.choices === '6. Exit'){
+    loop = false;
+}
+
+
+}while(loop)
+};
+
+sMs(allStudents)
